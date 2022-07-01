05a9b21696e50f9f6bcd3a0e6df3141434ddf4c8
Weronika Materna <wmaterna8@gmail.com>
2022-07-01 18:43:47 +0200

    remove unused files

--- src/main/kotlin/com/example/entities/order/OrderDraft.kt
+++ src/main/kotlin/com/example/entities/order/OrderDraft.kt
@@ -1,8 +1,5 @@
 package com.example.entities.order
 
-import com.example.entities.orderDetails.OrderDetails
-import com.example.entities.orderDetails.OrderDetailsResponse
-import com.example.entities.user.User
 
 data class OrderDraft(
     val id: Int,
